6a2bda1339ae062082cc3ce5ff060b1919eb022c
Huzefa <hchai52@gmail.com>
2020-08-10 19:32:05 +0530

    display bars

--- src/components/sortingVisualiser/SortingVisualiser.js
+++ src/components/sortingVisualiser/SortingVisualiser.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
-import styles from './SortingVisualiser.module.css';
-// create a component
+import './SortingVisualiser.css';
+
 class SortingVisualiser extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            array:[],
+        };
+    }
+
+    componentDidMount(){
+        this.resetArr();
+    }
+
+    resetArr(){
+        const array = [];
+        for (let i = 0; i < 100; i++) {
+            array.push(this.randomIntFromInterval(5, 600));
+        }
+    this.setState({array});
+    }
+
     render() {
         return (
-            <div>
-                <h1>
-                    Going to implement Sorting Visualiser
-                </h1>
+            <div className="container">
+                {this.state.array.map((value,i)=>(
+                    <div key={i} className="display-bar" style={{height:`${value}px`}}>
+                    </div>
+                ))}
             </div>
         );
     }
+
+    randomIntFromInterval(min, max) {
+        // min and max included
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
 }
 
-//make this component available to the app
 export default SortingVisualiser;
