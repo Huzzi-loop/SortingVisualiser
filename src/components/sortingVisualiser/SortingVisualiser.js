752a8e2bbbde0b72f24ee8dff15d28f5097ec414
Huzefa <hchai52@gmail.com>
2020-08-10 18:08:08 +0530

    Basic setting done

---
+++ src/components/sortingVisualiser/SortingVisualiser.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styles from './SortingVisualiser.module.css';
+// create a component
+class SortingVisualiser extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Going to implement Sorting Visualiser
+                </h1>
+            </div>
+        );
+    }
+}
+
+//make this component available to the app
+export default SortingVisualiser;
