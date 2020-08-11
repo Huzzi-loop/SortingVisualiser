635c4761ed9ce01c6e1f52c01c2bfcafe9425a57
Huzefa <hchai52@gmail.com>
2020-08-11 08:30:09 +0530

    Selection SOrt and bubble sort working

--- src/components/sortingAlgo/SelectionSort.js
+++ src/components/sortingAlgo/SelectionSort.js
@@ -1,4 +1,4 @@
-export function getBubbleSortAnimations(array) {
+export function getSelectionSortAnimations(array) {
     const animations = [];
     if (array.length <= 1) return array;
     doSort(array,animations);
@@ -9,18 +9,18 @@ export function getBubbleSortAnimations(array) {
 function doSort(array,animations){
     let swap;
     for(let i =0;i<array.length-1;i++){
-        for(let j =0;j<array.length-i-1;j++){
+        for(let j =i+1;j<array.length;j++){
             animations.push([i,j]);
             animations.push([i,j]);
-            if(array[j]<array[j+1]){
+            if(array[i]<array[j]){
+                animations.push([i,array[i]]);
                 animations.push([j,array[j]]);
-                animations.push([j+1,array[j+1]]);
             }else{
-                animations.push([j,array[j+1]]);
-                animations.push([j+1,array[j]]);
-                swap=array[j]
-                array[j]=array[j+1]
-                array[j+1]=swap
+                animations.push([i,array[j]]);
+                animations.push([j,array[i]]);
+                swap=array[i]
+                array[i]=array[j]
+                array[j]=swap
             }
         }
     }
--- src/components/sortingVisualiser/SortingVisualiser.js
+++ src/components/sortingVisualiser/SortingVisualiser.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './SortingVisualiser.css';
 import {getMergeSortAnimations} from '../sortingAlgo/MergeSort.js';
+import {getSelectionSortAnimations} from '../sortingAlgo/SelectionSort.js';
 import {getBubbleSortAnimations} from '../sortingAlgo/BubbleSort.js';
 
 const ANIMATION_SPEED_MS = 3;
@@ -60,6 +61,30 @@ class SortingVisualiser extends Component {
         }
     }
 
+    selectionSort(){
+        const animations = getSelectionSortAnimations(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+          const arrayBars = document.getElementsByClassName('display-bar');
+          const isColorChange = i % 4 < 2;
+          if (isColorChange) {
+            const [barOneIdx, barTwoIdx] = animations[i];
+            const barOneStyle = arrayBars[barOneIdx].style;
+            const barTwoStyle = arrayBars[barTwoIdx].style;
+            const color = i % 4 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+            setTimeout(() => {
+              barOneStyle.backgroundColor = color;
+              barTwoStyle.backgroundColor = color;
+            }, i * ANIMATION_SPEED_MS);
+          } else {
+            setTimeout(() => {
+              const [barOneIdx, newHeight] = animations[i];
+              const barOneStyle = arrayBars[barOneIdx].style;
+              barOneStyle.height = `${newHeight}px`;
+            }, i * ANIMATION_SPEED_MS);
+          }
+        }
+    }
+
     bubbleSort(){
         const animations = getBubbleSortAnimations(this.state.array);
         for (let i = 0; i < animations.length; i++) {
@@ -90,6 +115,7 @@ class SortingVisualiser extends Component {
             <>
                 <div>
                     <button onClick={()=>this.mergeSort()}>Merge Sort</button>
+                    <button onClick={()=>this.selectionSort()}>Selection Sort</button>
                     <button onClick={()=>this.bubbleSort()}>Bubble Sort</button>
                     <button onClick={()=>this.resetArr()}>Reset Array</button>
                 </div>
